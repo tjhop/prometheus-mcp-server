4ad3df832d7d3c771d14e9503fd83e53c4987393
TJ Hoplock <t.hoplock@gmail.com>
2025-07-03 00:52:51 -0400

    feat(promapi): set user-agent on the prometheus api client
    Sets user agent to `prometheus-mcp-server/$version
    (https://github.com/tjhop/prometheus-mcp-server)`, where $version comes
    from the internal version library with a version value populated by the
    linker during build.
    
    Shamelessly based almost entirely on example from client_golang docs.
    
    Signed-off-by: TJ Hoplock <t.hoplock@gmail.com>

--- pkg/prometheus/api.go
+++ pkg/prometheus/api.go
@@ -7,7 +7,32 @@ import (
 	"github.com/prometheus/client_golang/api"
 	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	config_util "github.com/prometheus/common/config"
+
+	"github.com/tjhop/prometheus-mcp-server/internal/version"
 )
+
+type userAgentRoundTripper struct {
+	name string
+	rt   http.RoundTripper
+}
+
+// RoundTrip implements the http.RoundTripper interface.
+func (u userAgentRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	if r.UserAgent() == "" {
+		// The specification of http.RoundTripper says that it shouldn't mutate
+		// the request so make a copy of req.Header since this is all that is
+		// modified.
+		r2 := new(http.Request)
+		*r2 = *r
+		r2.Header = make(http.Header)
+		for k, s := range r.Header {
+			r2.Header[k] = s
+		}
+		r2.Header.Set("User-Agent", u.name)
+		r = r2
+	}
+	return u.rt.RoundTrip(r)
+}
 
 func NewAPIClient(prometheusUrl, httpConfig string) (promv1.API, error) {
 	httpClient := http.DefaultClient
@@ -30,6 +55,10 @@ func NewAPIClient(prometheusUrl, httpConfig string) (promv1.API, error) {
 	client, err := api.NewClient(api.Config{
 		Client:  httpClient,
 		Address: prometheusUrl,
+		RoundTripper: userAgentRoundTripper{
+			name: fmt.Sprintf("prometheus-mcp-server/%s (https://github.com/tjhop/prometheus-mcp-server)", version.Version),
+			rt:   api.DefaultRoundTripper,
+		},
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create API client: %w", err)
