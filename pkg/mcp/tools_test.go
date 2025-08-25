c572fd749208aa4b29b19c044d85821befb6862b
TJ Hoplock <t.hoplock@gmail.com>
2025-08-25 18:02:26 -0400

    test: add tests for clean tombstones tool handler
    Signed-off-by: TJ Hoplock <t.hoplock@gmail.com>

--- pkg/mcp/tools_test.go
+++ pkg/mcp/tools_test.go
@@ -452,3 +452,69 @@ func TestDeleteSeriesToolHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestCleanTombstonesToolHandler(t *testing.T) {
+	testCases := []struct {
+		name                    string
+		request                 mcp.CallToolRequest
+		mockCleanTombstonesFunc func(ctx context.Context) error
+		validateResult          func(t *testing.T, result *mcp.CallToolResult, err error)
+	}{
+		{
+			name: "success",
+			request: mcp.CallToolRequest{
+				Request: mcp.Request{Method: string(mcp.MethodToolsCall)},
+				Params: mcp.CallToolParams{
+					Name: "clean_tombstones",
+				},
+			},
+			mockCleanTombstonesFunc: func(ctx context.Context) error {
+				return nil
+			},
+			validateResult: func(t *testing.T, result *mcp.CallToolResult, err error) {
+				require.NoError(t, err)
+				require.False(t, result.IsError)
+			},
+		},
+		{
+			name: "API error",
+			request: mcp.CallToolRequest{
+				Request: mcp.Request{Method: string(mcp.MethodToolsCall)},
+				Params: mcp.CallToolParams{
+					Name: "clean_tombstones",
+				},
+			},
+			mockCleanTombstonesFunc: func(ctx context.Context) error {
+				return errors.New("prometheus exploded")
+			},
+			validateResult: func(t *testing.T, result *mcp.CallToolResult, err error) {
+				require.NoError(t, err)
+				require.True(t, result.IsError)
+				require.Contains(t, toolCallResultAsString(result), "prometheus exploded")
+			},
+		},
+	}
+
+	mockAPI := &MockPrometheusAPI{}
+	mockServer := mcptest.NewUnstartedServer(t)
+	mockServer.AddTool(cleanTombstonesTool, cleanTombstonesToolHandler)
+
+	ctx := context.WithValue(context.Background(), apiClientKey{}, mockAPI)
+	err := mockServer.Start(ctx)
+	require.NoError(t, err)
+	defer mockServer.Close()
+
+	mcpClient := mockServer.Client()
+	defer mcpClient.Close()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockAPI.CleanTombstonesFunc = tc.mockCleanTombstonesFunc
+
+			res, err := mcpClient.CallTool(ctx, tc.request)
+			require.NoError(t, err)
+
+			tc.validateResult(t, res, err)
+		})
+	}
+}
