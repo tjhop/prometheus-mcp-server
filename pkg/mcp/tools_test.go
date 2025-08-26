c3c7ed39ebb1677c753e5b6885e1ed4245d715c1
TJ Hoplock <t.hoplock@gmail.com>
2025-08-25 22:50:48 -0400

    test: add tests for config tool handler
    Signed-off-by: TJ Hoplock <t.hoplock@gmail.com>

--- pkg/mcp/tools_test.go
+++ pkg/mcp/tools_test.go
@@ -894,3 +894,69 @@ func TestRuntimeinfoToolHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestConfigToolHandler(t *testing.T) {
+	testCases := []struct {
+		name           string
+		request        mcp.CallToolRequest
+		mockConfigFunc func(ctx context.Context) (promv1.ConfigResult, error)
+		validateResult func(t *testing.T, result *mcp.CallToolResult, err error)
+	}{
+		{
+			name: "success",
+			request: mcp.CallToolRequest{
+				Request: mcp.Request{Method: string(mcp.MethodToolsCall)},
+				Params: mcp.CallToolParams{
+					Name: "config",
+				},
+			},
+			mockConfigFunc: func(ctx context.Context) (promv1.ConfigResult, error) {
+				return promv1.ConfigResult{}, nil
+			},
+			validateResult: func(t *testing.T, result *mcp.CallToolResult, err error) {
+				require.NoError(t, err)
+				require.False(t, result.IsError)
+			},
+		},
+		{
+			name: "API error",
+			request: mcp.CallToolRequest{
+				Request: mcp.Request{Method: string(mcp.MethodToolsCall)},
+				Params: mcp.CallToolParams{
+					Name: "config",
+				},
+			},
+			mockConfigFunc: func(ctx context.Context) (promv1.ConfigResult, error) {
+				return promv1.ConfigResult{}, errors.New("prometheus exploded")
+			},
+			validateResult: func(t *testing.T, result *mcp.CallToolResult, err error) {
+				require.NoError(t, err)
+				require.True(t, result.IsError)
+				require.Contains(t, toolCallResultAsString(result), "prometheus exploded")
+			},
+		},
+	}
+
+	mockAPI := &MockPrometheusAPI{}
+	mockServer := mcptest.NewUnstartedServer(t)
+	mockServer.AddTool(configTool, configToolHandler)
+
+	ctx := context.WithValue(context.Background(), apiClientKey{}, mockAPI)
+	err := mockServer.Start(ctx)
+	require.NoError(t, err)
+	defer mockServer.Close()
+
+	mcpClient := mockServer.Client()
+	defer mcpClient.Close()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockAPI.ConfigFunc = tc.mockConfigFunc
+
+			res, err := mcpClient.CallTool(ctx, tc.request)
+			require.NoError(t, err)
+
+			tc.validateResult(t, res, err)
+		})
+	}
+}
